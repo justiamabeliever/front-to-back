1c9247e7059f913ba0c7de603c21dc19aa4bc18c
zhukevichs <alexandrzhukevich@gmail.com>
2019-10-11 03:15:35 +0300

    added register, login, jwt

--- server.js
+++ server.js
@@ -5,11 +5,20 @@ const app = express();
 
 connectDB();
 
+// Init middleware
+app.use(express.json({extended: false}));
+
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('API Running')
 })
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
